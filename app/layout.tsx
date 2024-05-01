18c680681c63516400d956b043b0ebbc33ad80c1
robin <techmaodao@gmail.com>
2024-05-01 14:35:29 +0800

    feat: new information

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
-                <ModeToggle />
-                <nav className="ml-auto text-sm font-medium space-x-6">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
+                <nav className="mr-auto font-medium space-x-6">
+                  <Link href="/" className="text-lg font-semibold">
+                    Dewall
+                  </Link>
+                  <Link className="text-sm" href="/">
+                    Home
+                  </Link>
+                  <Link className="text-sm" href="/about">
+                    About
+                  </Link>
                 </nav>
+                <ModeToggle />
               </div>
             </header>
             <main>{children}</main>
